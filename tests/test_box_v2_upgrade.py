0aa66a9e59b112e176482ee22e493753353c7f8b
Soo Hian <shonendumm@gmail.com>
2022-01-22 17:18:44 +0800

    add comment for pytest bug

--- tests/test_box_v2_upgrade.py
+++ tests/test_box_v2_upgrade.py
@@ -17,6 +17,9 @@ def test_contract_upgrade():
 
     box_v2 = BoxV2.deploy({"from": account})
     proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+
+    # there's a bug with ganache-cli which won't raise the VirtualMachineError
+    # need to start Ganache GUI app first, then run this test for exceptions
     with pytest.raises(exceptions.VirtualMachineError):
         proxy_box.increment({"from": account})
     upgrade(account, proxy, box_v2, proxy_admin_contract=proxy_admin)
